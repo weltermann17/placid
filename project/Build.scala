90103b69a57f81c89c0169fa56e3f8e6cc1f8fe9
weltermann17 <g-schmidt@de.ibm.com>
2015-03-05 08:46:34 +0100

    setting up build

--- project/Build.scala
+++ project/Build.scala
@@ -13,18 +13,17 @@ object CompleteBuild
   def buildSettings = Seq(
     organization := "placid",
     scalaVersion := Dependencies.Versions.scalaVersion,
-    crossScalaVersions := Dependencies.Versions.crossScalaVersions)
+    crossScalaVersions := Dependencies.Versions.crossScalaVersions,
+    ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet)
 
   def defaultSettings = buildSettings ++ baseSettings
 
-  lazy val root = Project(
-    id = "root",
-    base = file("."),
-    aggregate = Seq(util))
+  lazy val root = Project("root", file("."))
+    .aggregate(util)
 
-  lazy val util = Project(
-    id = "placid-util",
-    base = file("util"),
-    settings = defaultSettings ++ Seq(version in ThisBuild := "0.0.1-SNAPSHOT"))
+  lazy val util = Project("placid-util", file("util"))
+    .settings(defaultSettings: _*)
+    .settings(Seq(version in ThisBuild := "0.0.1-SNAPSHOT"): _*)
+    .settings(libraryDependencies ++= Dependencies.util)
 
 }
--- util/src/main/scala/placid/util/package.scala
+++ util/src/main/scala/placid/util/package.scala
@@ -1,7 +1,11 @@
 package placid
 
+import org.joda.time.DateTime
+
 package object util {
 
   val s = ""
+
+  def d = DateTime.parse("")
 
 }
