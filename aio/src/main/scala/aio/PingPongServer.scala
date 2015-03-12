9d68eb690d80f514cd9d3da35d51140deeb38146
weltermann17 <g-schmidt@de.ibm.com>
2015-03-12 20:30:04 +0100

    performance tests

--- aio/src/main/scala/aio/PingPongServer.scala
+++ aio/src/main/scala/aio/PingPongServer.scala
@@ -1,45 +1,76 @@
 package aio
 
 import java.net.{ InetSocketAddress, StandardSocketOptions }
+import java.net.StandardSocketOptions.{ SO_KEEPALIVE, SO_RCVBUF, SO_REUSEADDR, SO_SNDBUF, TCP_NODELAY }
+import java.nio.ByteBuffer
+import java.nio.channels.{ AsynchronousChannelGroup, AsynchronousServerSocketChannel, AsynchronousSocketChannel, CompletionHandler }
+import java.util.Collections
+import java.util.concurrent.{ AbstractExecutorService, TimeUnit, Executors }
 
-import java.nio.channels._
-import java.nio.charset._
-import java.nio._
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContextExecutorService
 
-class PingPongServer {
+object ExecutionContextExecutorServiceBridge {
+  def apply(ec: ExecutionContext): ExecutionContextExecutorService = ec match {
+    case null ⇒ throw null
+    case eces: ExecutionContextExecutorService ⇒ eces
+    case other ⇒ new AbstractExecutorService with ExecutionContextExecutorService {
+      @inline override def prepare(): ExecutionContext = other
+      @inline override def isShutdown = false
+      @inline override def isTerminated = false
+      @inline override def shutdown() = ()
+      @inline override def shutdownNow() = Collections.emptyList[Runnable]
+      @inline override def execute(runnable: Runnable): Unit = other execute runnable
+      @inline override def reportFailure(t: Throwable): Unit = other reportFailure t
+      @inline override def awaitTermination(length: Long, unit: TimeUnit): Boolean = false
+    }
+  }
+}
 
-  val server = AsynchronousServerSocketChannel
-    .open
+final class PingPongServer {
+
+  private[this] final val context = ExecutionContextExecutorServiceBridge(global)
+
+  private[this] final val server = AsynchronousServerSocketChannel
+    .open(AsynchronousChannelGroup.withFixedThreadPool(1, Executors.defaultThreadFactory))
     .setOption(StandardSocketOptions.SO_REUSEADDR, Boolean.box(true))
     .setOption(StandardSocketOptions.SO_RCVBUF, Integer.valueOf(64 * 1024))
-    .bind(new InetSocketAddress("localhost", 8080), 10000)
+    .bind(new InetSocketAddress("127.0.0.1", 8080), 10000)
 
   println(s"server started : $server")
   server.accept(null: Null, accepthandler)
 
-  object accepthandler extends CompletionHandler[AsynchronousSocketChannel, Null] {
+  private[this] object accepthandler extends CompletionHandler[AsynchronousSocketChannel, Null] {
 
-    def failed(e: Throwable, a: Null) = println(s"accept failed : $e")
+    @inline def failed(e: Throwable, a: Null) = println(s"accept failed : $e")
 
-    def completed(socket: AsynchronousSocketChannel, a: Null) = {
-
-      println(s"accept : $socket")
+    @inline def completed(socket: AsynchronousSocketChannel, a: Null) = {
 
       server.accept(null: Null, accepthandler)
 
-      val buffer = ByteBuffer.allocateDirect(64 * 1204)
+      socket.setOption(TCP_NODELAY, Boolean.box(true))
+      socket.setOption(SO_REUSEADDR, Boolean.box(true))
+      socket.setOption(SO_KEEPALIVE, Boolean.box(false))
+      socket.setOption(SO_RCVBUF, Integer.valueOf(64 * 1024))
+      socket.setOption(SO_SNDBUF, Integer.valueOf(64 * 1024))
 
-      type Handler = CompletionHandler[Integer, Null]
+      println(s"accept : $socket")
+
+      val readbuffer = ByteBuffer.allocateDirect(1024)
+
+      val writebuffer = response.duplicate
 
       object readhandler extends CompletionHandler[Integer, Null] {
 
-        def failed(e: Throwable, a: Null) = println(s"read failed : $e")
+        @inline def failed(e: Throwable, a: Null) = println(s"read failed : $e")
 
-        def completed(processed: Integer, a: Null) = {
+        @inline def completed(processed: Integer, a: Null) = {
 
-          if (0 < processed.intValue)
-            socket.write(response.duplicate(), null: Null, writehandler)
-          else {
+          if (960 == processed.intValue) {
+            writebuffer.rewind
+            socket.write(writebuffer, null: Null, writehandler)
+          } else {
             socket.close
             println(s"closed : $socket")
           }
@@ -50,24 +81,24 @@ class PingPongServer {
 
       object writehandler extends CompletionHandler[Integer, Null] {
 
-        def failed(e: Throwable, a: Null) = println(s"write failed : $e")
+        @inline def failed(e: Throwable, a: Null) = println(s"write failed : $e")
 
-        def completed(processed: Integer, a: Null) = {
+        @inline def completed(processed: Integer, a: Null) = {
 
-          buffer.clear
-          socket.read(buffer, null: Null, readhandler)
+          readbuffer.clear
+          socket.read(readbuffer, null: Null, readhandler)
 
         }
 
       }
 
-      socket.read(buffer, null: Null, readhandler)
+      socket.read(readbuffer, null: Null, readhandler)
 
     }
 
   }
 
-  val response: ByteBuffer = {
+  private[this] final val response: ByteBuffer = {
     val txt = """HTTP/1.1 200 OK
 Connection: keep-alive
 Content-Type: text/plain
@@ -75,8 +106,8 @@ Content-Length: 4
 Date: Wed, 11 Mar 2015 13:13:24 GMT
 
 pong""".getBytes
-    val buf = ByteBuffer.allocateDirect(200)
-    buf.put(txt)
+    val buf = ByteBuffer.allocateDirect(64 * 1024)
+    for (_ ← 1 to 24) buf.put(txt)
     buf.flip
     buf
   }
